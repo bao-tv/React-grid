756e5146a4c712bca6b13c84e54e8fda5c7910d0
Viet Bao <vietbao.t@gmail.com>
2024-03-13 17:03:12 +0700

    add da ta

--- src/Test.jsx
+++ src/Test.jsx
@@ -7,19 +7,23 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const Test = () => {
     const [state, setState] = useState([
-        { i: "1", x: 0, y: 0, w: 1, h: 2, static: true },
-        { i: "2", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-        { i: "3", x: 4, y: 0, w: 1, h: 2 }
+        { i: "1", x: 0, y: 0, w: 1, h: 1, static: true, data:{id: 1, name: 1, barcode: 1 } },
+        { i: "2", x: 1, y: 0, w: 1, h: 1, data:{id: 2, name: 2, barcode: 2 } },
+        { i: "3", x: 0, y: 1, w: 1, h: 1, data:{id: 3, name: 3, barcode: 3 } }
     ]);
 
     console.log('bao state: ', state);
     
     const handleAdd = () => {
         setState(prevState => {
-            const newItem = { i: prevState.length.toString(), x: 0, y: 0, w: 2, h: 2 };
+            const newItem = { i: prevState.length + 1, x: prevState.length % 2 === 0 ? 0 : 1, y: 1, w: 1, h: 1, data:{id: prevState.length + 1, name: prevState.length + 1, barcode: prevState.length + 1 } };
             return [...prevState, newItem];
         });
     }
+
+    const onLayoutChange = (layout, layouts) => {
+        console.log(layouts)
+      }
 
     return (
         <>
@@ -30,13 +34,18 @@ const Test = () => {
                     <ResponsiveGridLayout
                         className="layout border border-dark-subtle rounded"
                         layout={state}
-                        rowHeight={30}
+                        rowHeight={120}
                         width={300}
+                        onLayoutChange={(layout, layouts) => onLayoutChange(layout, layouts)}
                         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
                     >
                         {state.map(item => (
-                            <div key={item.i} style={{ border: '1px solid' }}>{item.i}</div>
+                            <div key={item.i} style={{ border: '1px solid' }} data-grid={item}>
+                                <p>ID: <span>{item.data.id}</span></p>
+                                <p>Name: <span>{item.data.id}</span></p>
+                                <p>Barcode: <span>{item.data.id}{item.data.id}{item.data.id}</span></p>
+                            </div>
                         ))}
                     </ResponsiveGridLayout>
                 </div>
